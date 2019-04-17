2c4eb861309bfc73c51b1502846a0d950c5df728
James Brown <jamesphone180@gmail.com>
2019-04-17 13:52:41 +1200

    Added matrix creation function for rotations.

--- src/brownshome/vecmath/Rot3.java
+++ src/brownshome/vecmath/Rot3.java
@@ -26,4 +26,16 @@ public interface Rot3 extends Vec4 {
 		double dot = dot(o);
 		return Math.acos(2 * dot * dot - 1);
 	}
+
+	/** Returns a matrix R such as R * p = p' */
+	default double[][] createMatrix() {
+		double x2 = x() * x(), y2 = y() * y(), z2 = z() * z();
+
+		// Taken from https://en.wikipedia.org/wiki/Quaternions_and_spatial_rotation#Quaternion-derived_rotation_matrix
+		return new double[][] {
+			{ 1 - 2 * (y2 + z2),           2 * (x() * y() - z() * w()), 2 * (x() * z() + y() * w()) },
+			{ 2 * (x() * y() + z() * w()), 1 - 2 * (x2 + z2),           2 * (y() * z() - x() * w()) },
+			{ 2 * (x() * z() - y() * w()), 2 * (y() * z() + x() * w()), 1 - 2 * (x2 + y2)           }
+		};
+	}
 }
